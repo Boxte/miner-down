245e65ee9f12f22eece456aa736e89b7a520f8f0
Van <toasted.crunchy.almond@gmail.com>
2022-07-08 11:01:56 -0700

    feat: return JSON response

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const url = `https://hiveon.net/eth/overview?miner=${process.env.MINER_ADDRESS}`
 const activeWorkersSelector = `#next-focus-wrapper > div.Page-module-root-1YEf0 > div > div > section:nth-child(4) > div > div.Overview_widgets__yUK6Y > div:nth-child(3) > div.Overview_widgetContent__31FBw > div.Overview_activeCounter__1fj9O > span:nth-child(1)`;
 
 exports.handler = async (event) => {
+    const utcRequestTimestamp = new Date().getTime();
     const browser = await chromium.puppeteer
         .launch({
             args: chromium.args,
@@ -17,12 +18,16 @@ exports.handler = async (event) => {
 
     await page.goto(url);
 
-    var element = await page.waitForSelector(activeWorkersSelector);
-    var text = await page.evaluate(element => element.textContent, element);
+    const element = await page.waitForSelector(activeWorkersSelector);
+    const numberOfActiveWorkers = await page.evaluate(element => element.textContent, element);
 
     if (browser !== null) {
         await browser.close();
     }
 
-    return `Number of active workers: ${text}`;
+    const utcResponseTimestamp = new Date().getTime();
+    const utcReqResDifference = utcResponseTimestamp - utcRequestTimestamp;
+    let jsonData = { utcRequestTimestamp, numberOfActiveWorkers, utcResponseTimestamp, utcReqResDifference };
+
+    return jsonData;
 }
