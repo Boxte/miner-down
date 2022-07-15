b24f38283fe8da302a1655d30cdb5fd6a1b30be9
Van <toasted.crunchy.almond@gmail.com>
2022-07-14 23:28:53 -0700

    feat: added discord webhook

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 // This version of chromium works wil Node 14.x on AWS Lambda
-const chromium = require('chrome-aws-lambda');
-require('dotenv').config();
+const chromium = require("chrome-aws-lambda");
+require("dotenv").config();
+const Discord = require("discord.js");
 
 const url = `https://hiveon.net/eth/overview?miner=${process.env.MINER_ADDRESS}`;
 const activeWorkersSelector = `#next-focus-wrapper > div.Page-module-root-1YEf0 > div > div > section:nth-child(4) > div > div.Overview_widgets__yUK6Y > div:nth-child(3) > div.Overview_widgetContent__31FBw > div.Overview_activeCounter__1fj9O > span:nth-child(1)`;
 
 exports.handler = async (event) => {
-    const utcRequestTimestamp = new Date().getTime();
-    const browser = await chromium.puppeteer
-        .launch({
-            args: chromium.args,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless
-        });
-    const page = await browser.newPage();
+  const utcRequestTimestamp = new Date().getTime();
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
+  const page = await browser.newPage();
 
-    await page.goto(url);
+  await page.goto(url);
 
-    const element = await page.waitForSelector(activeWorkersSelector);
-    const numberOfActiveWorkers = await page.evaluate(element => element.textContent, element);
+  const element = await page.waitForSelector(activeWorkersSelector);
+  const numberOfActiveWorkers = await page.evaluate(
+    (element) => element.textContent,
+    element
+  );
 
-    if (browser !== null) {
-        await browser.close();
-    }
+  if (browser !== null) {
+    await browser.close();
+  }
 
-    const utcResponseTimestamp = new Date().getTime();
-    const utcReqResDifference = utcResponseTimestamp - utcRequestTimestamp;
-    let jsonData = { utcRequestTimestamp, numberOfActiveWorkers, utcResponseTimestamp, utcReqResDifference };
+  const utcResponseTimestamp = new Date().getTime();
+  const utcReqResDifference = utcResponseTimestamp - utcRequestTimestamp;
+  let jsonData = {
+    utcRequestTimestamp,
+    numberOfActiveWorkers,
+    utcResponseTimestamp,
+    utcReqResDifference,
+  };
 
-    return jsonData;
-}
+  const webhook = new Discord.WebhookClient(
+    process.env.WEBHOOK_ID,
+    process.env.WEBHOOK_TOKEN
+  );
+
+  const { sendWebhook } = event;
+  if (sendWebhook) {
+    const msg = `Number of active workers: ${numberOfActiveWorkers}\nTime for response: ${
+      utcReqResDifference / 1000
+    }s`;
+    webhook.send(msg).catch(console.error);
+  }
+
+  return jsonData;
+};
