ea077e06bfe02d5823c0f3d8104a3f2e2984c946
vl <levane@vt.edu>
2022-08-28 11:40:57 -0700

    chore: update selector because hiveon updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const chromium = require("chrome-aws-lambda");
 require("dotenv").config();
 
 const url = `https://hiveon.net/eth/overview?miner=${process.env.MINER_ADDRESS}`;
-const activeWorkersSelector = `#next-focus-wrapper > div.Page-module-root-1YEf0 > div > div > section:nth-child(4) > div > div.Overview_widgets__yUK6Y > div:nth-child(3) > div.Overview_widgetContent__31FBw > div.Overview_activeCounter__1fj9O > span:nth-child(1)`;
+const activeWorkersSelector = `#next-focus-wrapper > div.Page-module-root-1YEf0 > div > div > section:nth-child(5) > div > div.Overview_widgets__yUK6Y > div:nth-child(3) > div.Overview_widgetContent__31FBw > div.Overview_activeCounter__1fj9O > span:nth-child(1)`;
 
 /**
  *
